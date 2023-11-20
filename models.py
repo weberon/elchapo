e34878e1a9317a8d375af31b2c1cdf49773114b1
Akshay H <akshay@weberon.net>
2023-11-20 11:57:54 +0000

    Fix: Indentation in file

--- models.py
+++ models.py
@@ -5,8 +5,8 @@ from datetime import datetime
 from os import getenv
 from pytz import UTC
 from pynamodb.attributes import (
-    MapAttribute,
-    UnicodeAttribute, UTCDateTimeAttribute, BooleanAttribute
+		MapAttribute,
+		UnicodeAttribute, UTCDateTimeAttribute, BooleanAttribute
 )
 from pynamodb.models import Model
 
@@ -20,33 +20,33 @@ def get_now():
 	return datetime.utcnow().replace(tzinfo=UTC)
 
 def create_table_name(table_name):
-    """
-    create table to name for dynamodb
-    """
-    return '%s-%s' % (ENV, table_name)
+		"""
+		create table to name for dynamodb
+		"""
+		return '%s-%s' % (ENV, table_name)
 
 
 class ShortURL(Model):
-    """
-    A DynamoDB VideoWatch
-    """
+	"""
+	A DynamoDB VideoWatch
+	"""
 
-    class Meta:
-        """
-        set meta of table
-        """
-        table_name = create_table_name("el-chapo-short-url-store")
-        region = AWS_REGION
+	class Meta:
+		"""
+		set meta of table
+		"""
+		table_name = create_table_name("el-chapo-short-url-store")
+		region = getenv("environ")
 
-    url = UnicodeAttribute(hash_key=True, null=False)
-    redirection_url = UnicodeAttribute(null=False)
-    webhook = UnicodeAttribute(null=True)
-    created_at = UTCDateTimeAttribute(default=get_now())
+	url = UnicodeAttribute(hash_key=True, null=False)
+	redirection_url = UnicodeAttribute(null=False)
+	webhook = UnicodeAttribute(null=True)
+	created_at = UTCDateTimeAttribute(default=get_now())
 
 
 # automatically creates a table in dynamo db if doesnt exist.
 if not ShortURL.exists():
-    ShortURL.create_table(wait=True, billing_mode="PAY_PER_REQUEST")
+	ShortURL.create_table(wait=True, billing_mode="PAY_PER_REQUEST")
 
 # A DynamoDB Model for request logging
 class RequestLogger(Model):
